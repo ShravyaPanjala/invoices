0481b6054374663202254737613f7307ca1efc13
ShravyaPanjala <shravya.panjala@gmail.com>
2019-01-02 02:37:21 +0530

    added middleware

---
+++ build/precache-manifest.6e9cc355db37fd4fc131cc845177bb6b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "157c0241a6946bc4e5d2",
+    "url": "/static/js/main.157c0241.chunk.js"
+  },
+  {
+    "revision": "392017ceb1be3a7f763a",
+    "url": "/static/js/1.392017ce.chunk.js"
+  },
+  {
+    "revision": "157c0241a6946bc4e5d2",
+    "url": "/static/css/main.5747c18b.chunk.css"
+  },
+  {
+    "revision": "23e1d656ed67c16fe80b214ba59342ef",
+    "url": "/index.html"
+  }
+];
